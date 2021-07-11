84a933560896dfc1a175118f7f8082a2db764cdf
Sahil Bhatia <sahilofficial671@gmail.com>
2021-07-11 12:55:11 +0530

    Change to test pipeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
                             Get started
                         </a>
                     @endguest
-
                     @auth
                         <a href="{{ route('dashboard') }}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10">
                             Go to Dashboard
